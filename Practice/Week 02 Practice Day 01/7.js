b69527d0b89a68ac2b6879f16b56143aecdd6803
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-05-17 22:15:01 +0600

    Add implementation to filter unique numbers from an array

---
+++ Practice/Week 02 Practice Day 01/7.js
@@ -0,0 +1,15 @@
+var numbers = [1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10];
+
+let unique = []
+
+for(let i=0; i<numbers.length; i++) {
+
+    if(unique.find(num => num == numbers[i] ) == undefined) {
+
+        unique.push(numbers[i]);
+
+    }
+}
+
+
+console.log(unique);
